f8effda226df30a72305e1938fa35584454d12ea
imseanwang1123 <yochen1041@gmail.com>
2021-03-14 23:35:53 +0800

    factory method pattern

---
+++ src/main/java/design_pattern/factory_method_pattern/FactoryMethodPattern.java
@@ -0,0 +1,49 @@
+package design_pattern.factory_method_pattern;
+
+public class FactoryMethodPattern {
+
+    public static void main(String[] args) {
+        Loader jsonLoader = new JsonLoaderFactory().createLoader();
+        jsonLoader.load();
+
+        Loader xmlLoader = new XmlLoaderFactory().createLoader();
+        xmlLoader.load();
+
+    }
+
+    public interface LoaderFactory {
+        Loader createLoader();
+    }
+
+    public static class JsonLoaderFactory implements LoaderFactory {
+        @Override
+        public Loader createLoader() {
+            return new JsonLoader();
+        }
+    }
+
+    public static class XmlLoaderFactory implements LoaderFactory {
+        @Override
+        public Loader createLoader() {
+            return new XmlLoader();
+        }
+    }
+
+    public interface Loader {
+        void load();
+    }
+
+    public static class JsonLoader implements Loader {
+        @Override
+        public void load() {
+            System.out.println("Load Json");
+        }
+    }
+
+    public static class XmlLoader implements Loader {
+        @Override
+        public void load() {
+            System.out.println("Load Xml");
+        }
+    }
+}
