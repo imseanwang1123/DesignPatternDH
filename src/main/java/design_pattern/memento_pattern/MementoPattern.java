952df2e0386f874965d0a5cdef2f229e02b41706
seanwang <>
2021-03-24 16:11:27 +0800

    memento pattern

---
+++ src/main/java/design_pattern/memento_pattern/MementoPattern.java
@@ -0,0 +1,45 @@
+package design_pattern.memento_pattern;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+public class MementoPattern {
+
+    @AllArgsConstructor
+    public static class Memento {
+
+        @Getter
+        @Setter
+        private final String state;
+    }
+
+    public static class Originator {
+
+        @Getter
+        private String state;
+
+        public void setState(String state) {
+            System.out.println(state + "!");
+            this.state = state;
+        }
+
+        public Memento createMemento() {
+            return new Memento(this.state);
+        }
+
+        public void restoreMemento(Memento memento) {
+            System.out.println("restore" + memento.getState());
+            this.state = memento.getState();
+        }
+    }
+
+    public static class Caretaker {
+
+        @Getter
+        @Setter
+        private Memento memento;
+    }
+
+
+}
